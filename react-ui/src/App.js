d1ca0b8fe8fd1d6d7f33775e4109cec8c80ea0c2
Kat Reinhart <kat.reinhart@gmail.com>
2017-07-19 11:54:14 -0700

    Start work on front end

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
+import Header from './Header';
+
+import uriPath from '../config.js';
+
+const URI = process.env.URI || uriPath;
 
 export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      pageHeader: "Preference Center",
+      url: `${URI}/api/prefs/596ea67a92ee13b276b283be`
+    }
+  }
   render() {
     return(
-      <div>Hello React!</div>
+      <Header message={this.state.pageHeader}/>
     )
   }
 }
