dbc9011211f1ca887f1c3e6fc15379c4c1b5d1fc
Kat Reinhart <kat.reinhart@gmail.com>
2017-07-19 12:05:35 -0700

    Trying again

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react';
 import Header from './Header';
 
 if(process.env === 'development'){
-    const URI = require('../config.js')
+    const URL = require('../config.js')
 }
 
 else {
-    const URI = process.env.URI;
+    const URL = process.env.URI;
 }
 
 
@@ -14,8 +14,8 @@ export default class App extends Component {
   constructor() {
     super();
     this.state = {
-      pageHeader: "Preference Center"//,
-      // url: `${URI}/api/prefs/596ea67a92ee13b276b283be`
+      pageHeader: "Preference Center",
+      url: `${URL}/api/prefs/596ea67a92ee13b276b283be`
     }
   }
   render() {
